7904f1336f720d854b9e4179cdbe551a9f98ad23
abhishekbora09 <147861205+abhishekbora09@users.noreply.github.com>
2023-12-20 18:20:15 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,7 +6,7 @@ from datasets import load_dataset
 
 st.set_page_config(layout='wide', initial_sidebar_state='collapsed')
 
-@st.cache(allow_input_mutation=True, suppress_st_warning=True)
+@st.cache(allow_input_mutation=True)
 def load_data():
     # df_original = pd.read_csv("./data/github_dataset.csv")
     # df = df_original.copy()
